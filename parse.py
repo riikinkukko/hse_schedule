368c428a22d179e8cd344e14bccf651139ac31bb
Denischerov2312 <107198033+Denischerov2312@users.noreply.github.com>
2025-09-23 03:27:38 +0300

    Добавил функцию создания запроса и добавил в нее проверки

--- parse.py
+++ parse.py
@@ -1,8 +1,34 @@
-from bs4 import soup
+import requests
+from time import sleep
+from bs4 import BeautifulSoup as Soup
+
+PARSING_URL = 'https://docs.google.com/spreadsheets/d/1BMR4Zk3BU2Tyo7L-CYJeMfVuCxjmmT94kfz4Jp6BFAc/edit?gid=739453176#gid=739453176'
 
 
+def check_for_redirect(response):
+    if response.history:
+        raise requests.HTTPError
+
+
+def get_response(url):
+    while True:
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            check_for_redirect(response)
+            return response
+        except ConnectionError:
+            print('Подключение отсутствует')
+            sleep(15)
+
+
+def parse():
+    pass
+
+   
 def main():
     pass
 
+
 if __name__ == '__main__':
-    main()
+    parse()
